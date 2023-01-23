6b62cdeb7b0d2edfc8e9cc42f05130551dac317d
Yash Raj <Yash@google.com>
2023-01-24 00:50:25 +0530

    now the images are draggable

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ function displayImages() {
                 </div>`;
   });
   output.innerHTML = images;
+  jqueryMotion();
 }
 
 col.addEventListener("change", () => {
@@ -49,4 +50,58 @@ function widthDeterminer() {
   redSquare.style.height = `${
     Math.ceil(lengthofFiles / col.value) * 225 - 25
   }px`;
+}
+
+function jqueryMotion() {
+  $(document).ready(function () {
+    var $bg = $(".img"),
+      origin = { x: 0, y: 0 },
+      start = { x: 0, y: 0 },
+      movecontinue = false;
+
+    function move(e) {
+      var moveby = {
+        x: origin.x - e.clientX,
+        y: origin.y - e.clientY,
+      };
+
+      if (movecontinue === true) {
+        start.x = start.x - moveby.x;
+        start.y = start.y - moveby.y;
+
+        $(this).css("background-position", start.x + "px " + start.y + "px");
+      }
+
+      origin.x = e.clientX;
+      origin.y = e.clientY;
+
+      e.stopPropagation();
+      return false;
+    }
+
+    function handle(e) {
+      movecontinue = false;
+      $bg.unbind("mousemove", move);
+
+      if (e.type == "mousedown") {
+        origin.x = e.clientX;
+        origin.y = e.clientY;
+        movecontinue = true;
+        $bg.bind("mousemove", move);
+      } else {
+        $(document.body).focus();
+      }
+
+      e.stopPropagation();
+      return false;
+    }
+
+    function reset() {
+      start = { x: 0, y: 0 };
+      $(this).css("backgroundPosition", "0 0");
+    }
+
+    $bg.bind("mousedown mouseup mouseleave", handle);
+    $bg.bind("dblclick", reset);
+  });
 }
